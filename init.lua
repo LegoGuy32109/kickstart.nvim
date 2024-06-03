1e4f6f12bad1d30d9d9380d5d9a9ccb28d12ce4d
LegoGuy32109 <joshevanhale@gmail.com>
2024-06-02 22:05:32 -0400

    followed a tut for autosaving stuff

--- init.lua
+++ init.lua
@@ -112,6 +112,8 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader><leader>', '<cmd>nohlsearch<CR>')
 
+vim.keymap.set('n', '<leader><leader>x', '<cmd>w<CR><cmd>source %<CR>', { silent = true })
+
 -- My preferred way to escape modes
 vim.keymap.set('i', 'jk', '<Esc>')
 
@@ -328,7 +330,7 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
